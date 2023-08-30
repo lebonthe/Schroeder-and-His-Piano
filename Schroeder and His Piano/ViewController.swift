cb4a21f24ca86aa04d527c50108277823a4b8a2d
Min Hu <lebonthe@gmail.com>
2023-08-30 16:16:56 +0800

    Initial Commit

---
+++ Schroeder and His Piano/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Schroeder and His Piano
+//
+//  Created by Min Hu on 2023/8/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
